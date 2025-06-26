cb90c3ec8a8fb1957c55e14d389111dfdd2585ae
Omid Afifi <afifiomid@yahoo.com>
2025-06-26 20:36:25 +0330

    Template String

---
+++ array_methods_exercises/pop_example.js
@@ -0,0 +1,6 @@
+// pop_example
+let names = ["Omidreza", "Dorna", "Iman"];
+names.pop();
+console.log(names);
+let removedName = names.pop();
+console.log(removedName);
---
+++ array_methods_exercises/template.js
@@ -0,0 +1,6 @@
+//Template String or Template Literal Template String نوعی روش چاپ متغیرها و متن‌ها به‌شکل تمیز و خوانا هست که با بک‌تیک (`) نوشته می‌شه، نه با “ ی
+console.log(`ادامه متن   {33}$   متن ثابت   `);
+let firstName = "omidreza";
+let lastName = "Afifi";
+let age = 33;
+console.log(`${firstName} ${lastName} ${age} ${`Jahre alt`}`);
