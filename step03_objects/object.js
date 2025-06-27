83670c4ed79372f721211846c0f5781ac14c2d1b
Omid Afifi <afifiomid@yahoo.com>
2025-06-27 16:26:27 +0330

    . / Objects

---
+++ step03_objects/object.js
@@ -0,0 +1,19 @@
+// Objects
+// let courses = {
+//   languages: ["Html", "Css", "tailwindCss", "JavaScript"],
+//   time: 23,
+// };
+// console.log(courses);
+
+const courses = {
+  fullName: "javascript course , Reac , Html and Css",
+  time: 32,
+};
+console.log(courses);
+console.log(courses.fullName);
+console.log(courses["time"]);
+
+//
+let student = { firstName: "omidreza", lasName: "Afifi", age: 34 };
+console.log(student);
+console.log(student.firstName, student["lasName"], student["age"]);
