b1a9b0471fddf4115c3a4bdbb372deedc7153842
Omid Afifi <afifiomid@yahoo.com>
2025-06-27 15:24:02 +0330

    pop and includes

---
+++ array_methods_exercises/includes_example.js
@@ -0,0 +1,6 @@
+let heute = ["html", "css", "javascript"];
+if (heute.includes("javascript")) {
+  console.log("JavaScript ist verfügbar");
+} else {
+  console.log("nix");
+}
--- array_methods_exercises/pop_example.js
+++ array_methods_exercises/pop_example.js
@@ -4,3 +4,8 @@ names.pop();
 console.log(names);
 let removedName = names.pop();
 console.log(removedName);
+//
+pop;
+let value = ["sara", " Nazi ", "karim"];
+let removedName = value.pop();
+alert(removedName);
