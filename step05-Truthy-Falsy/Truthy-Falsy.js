b75e25f5ddb75cf8a8158505e02e4261dd632ab3
Omid Afifi <afifiomid@yahoo.com>
2025-06-28 19:15:37 +0330

    Truthy and Falsy Values

---
+++ step05-Truthy-Falsy/Truthy-Falsy.js
@@ -0,0 +1,32 @@
+//Truthy and Falsy Values
+console.log(true ? "truthy" : "falsy");
+console.log(false ? "truthy" : "falsy");
+//boolean ==>true and false
+// Sting||boolean||number||Nan|| undefined/object/Array==> Truthy and Falsy
+console.log(0 ? "truthy" : "falsy");
+console.log(-3252123123 ? "truthy" : "falsy");
+console.log(1 ? "truthy" : "falsy");
+console.log("Ali" ? "truthy" : "falsy");
+console.log("" ? "truthy" : "falsy");
+console.log(0 / 1 ? "truthy" : "falsy");
+console.log(12315612312 ? "truthy" : "falsy");
+console.log(NaN ? "truthy" : "falsy");
+console.log(undefined ? "truthy" : "falsy");
+console.log(true ? "truthy" : "falsy");
+console.log(false ? "truthy" : "falsy");
+console.log([] ? "truthy" : "falsy");
+console.log(["Ali", "Mohammad", 32, true, false] ? "truthy" : "falsy");
+console.log({} ? "truthy" : "falsy");
+console.log(
+  {
+    firstName: "omidreza",
+    lastName: "Afifi",
+    age: 34,
+    email: "omidrzafifi@gmail.com",
+  }
+    ? "truthy"
+    : "falsy"
+);
+//*==> soal Mosahebe hamishe umade(important)
+//Falsy ==> sting " " , 0 , undefined ,Null ,fals
+//Truthy ==> Har chiz be gheir az inha
