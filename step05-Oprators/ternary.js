51b420e479385fa40d122521d6619052f35fd299
Omid Afifi <afifiomid@yahoo.com>
2025-06-28 18:14:06 +0330

    Ternary Operators

---
+++ step05-Oprators/ternary.js
@@ -0,0 +1,18 @@
+// Ternary Operators ==>fekr konam behesh migan amalgar haye 3gane
+// Kheili por karbord hast makhsusan vaghti berid soragh react
+//dar nullish 0 ??0  ama ternary 0 ? 0 : hast
+//            shart(true or false) ?agar (true bashe true mide) : (vali agar shart false bahe inja be man neshun mide)
+// gerefti chi shod
+// in kheili mohem hast
+let x = 5;
+let y = 6;
+console.log(x < y);
+// x < y ? "yes" : "no";
+console.log(true ? "yes" : "no");
+console.log(false ? "yes" : "no");
+//
+console.log(x < y ? "yes" : "no");
+console.log(x > y ? "yes" : "no");
+console.log(x <= y ? "yes" : "no");
+console.log(x >= y ? "yes" : "no");
+//kheili estefade mishe
