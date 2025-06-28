f4a70a9bd300b7b72f992668cb3453385cdfc29b
Omid Afifi <afifiomid@yahoo.com>
2025-06-28 16:14:05 +0330

    Comparison Oprators

--- step05-Oprators/comparison.js
+++ step05-Oprators/comparison.js
@@ -2,5 +2,20 @@
 
 const x = 5;
 const y = 3;
-const z = 5;
+const z = "5";
 console.log(x == y);
+console.log(x == z);
+console.log(x == z);
+// * pas vaghti ma == mizarim 2 ta meghdar ba ham barasi mishe na type
+console.log(x === z);
+console.log(x != y);
+console.log(x != z);
+// Pas nokte Saay konid hamishe 3 ta mosavi estefade konid .
+//1 ki az Soalat JavaScript === and == and = hast
+//! =  ! == ! === (!) in not hast
+console.log(x !== z);
+//
+console.log(x < y);
+console.log(x > y);
+console.log(2 > 2);
+console.log(2 >= 2);
