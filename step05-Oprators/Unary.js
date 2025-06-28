777118987f32234f48b07ae1bb43b35797ba0071
Omid Afifi <afifiomid@yahoo.com>
2025-06-28 17:17:00 +0330

    Unary Oprators

---
+++ step05-Oprators/Unary.js
@@ -0,0 +1,10 @@
+// Unary
+let value1 = "5";
+console.log(5 + value1);
+console.log(+value1); // inja reshte be adad tabdil shod
+//Unery Plus dar emtehanat miad +(var) <== **
+let value2 = 5;
+console.log(5 + value2);
+// Unary negation miad adadha ro az mosbat be manfi va az manfi be mosbat taghir mide 
+console.log(-value1);
+console.log(-value2);
