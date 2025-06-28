9e9247716c3ef1e9de210a3c125c85f562e39503
Omid Afifi <afifiomid@yahoo.com>
2025-06-28 17:05:46 +0330

    String Oprators

---
+++ step05-Oprators/sting.js
@@ -0,0 +1,9 @@
+//String Oprators
+let firstName = "Omidreza";
+let lastName = "Afifi";
+let finalText = "Hi im am " + firstName + ` ` + lastName;
+console.log(finalText);
+// firstName = firstName + ` ` + lastName;
+// console.log(firstName);
+firstName += lastName;
+console.log(firstName);
