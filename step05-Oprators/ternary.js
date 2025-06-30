7b581bfd7918afa8d035c21e1dac76f26e610850
Omid Afifi <afifiomid@yahoo.com>
2025-06-30 19:50:11 +0330

    Turnary Oprator

--- step05-Oprators/ternary.js
+++ step05-Oprators/ternary.js
@@ -1,49 +1,64 @@
-// Ternary Operators ==>fekr konam behesh migan amalgar haye 3gane
-// Kheili por karbord hast makhsusan vaghti berid soragh react
-//dar nullish 0 ??0  ama ternary 0 ? 0 : hast
-//            shart(true or false) ?agar (true bashe true mide) : (vali agar shart false bahe inja be man neshun mide)
-// gerefti chi shod
-// in kheili mohem hast
-let x = 5;
-let y = 6;
-console.log(x < y);
-// x < y ? "yes" : "no";
-console.log(true ? "yes" : "no");
-console.log(false ? "yes" : "no");
-//
-console.log(x < y ? "yes" : "no");
-console.log(x > y ? "yes" : "no");
-console.log(x <= y ? "yes" : "no");
-console.log(x >= y ? "yes" : "no");
-//kheili estefade mishe
+// // Ternary Operators ==>fekr konam behesh migan amalgar haye 3gane
+// // Kheili por karbord hast makhsusan vaghti berid soragh react
+// //dar nullish 0 ??0  ama ternary 0 ? 0 : hast
+// //            shart(true or false) ?agar (true bashe true mide) : (vali agar shart false bahe inja be man neshun mide)
+// // gerefti chi shod
+// // in kheili mohem hast
+// let x = 5;
+// let y = 6;
+// console.log(x < y);
+// // x < y ? "yes" : "no";
+// console.log(true ? "yes" : "no");
+// console.log(false ? "yes" : "no");
+// //
+// console.log(x < y ? "yes" : "no");
+// console.log(x > y ? "yes" : "no");
+// console.log(x <= y ? "yes" : "no");
+// console.log(x >= y ? "yes" : "no");
+// //kheili estefade mishe
 
-//
-let age = 20;
-console.log(age >= 20 ? "Adult" : "Minor");
-//
+// //
+// let age = 20;
+// console.log(age >= 20 ? "Adult" : "Minor");
+// //
 
-let isLoggedIn = false;
-// console.log((isLoggedIn = true ? "Welcome back" : "Please log in"));
-console.log(isLoggedIn ? "Welcome back" : "Please log in");
-//
-// 📌 پس همیشه این قانون رو یادت باشه:
+// let isLoggedIn = false;
+// // console.log((isLoggedIn = true ? "Welcome back" : "Please log in"));
+// console.log(isLoggedIn ? "Welcome back" : "Please log in");
+// //
+// // 📌 پس همیشه این قانون رو یادت باشه:
 
-// اگر متغیر خود به‌تنهایی boolean (درست یا غلط) باشه، دیگه نیاز نیست بنویسی === true یا === false
+// // اگر متغیر خود به‌تنهایی boolean (درست یا غلط) باشه، دیگه نیاز نیست بنویسی === true یا === false
 
-//
-let accountBalance = 100;
-let finalPrice = 120;
-console.log(
-  accountBalance >= finalPrice ? "You can buy it!" : "Not enough balance."
-);
+// //
+// let accountBalance = 100;
+// let finalPrice = 120;
+// console.log(
+//   accountBalance >= finalPrice ? "You can buy it!" : "Not enough balance."
+// );
 
+// // let score = 85;
+// // console.log(score>90?"Grade: A"?score===70&&89"Grade: B":"Grade: C");
 // let score = 85;
-// console.log(score>90?"Grade: A"?score===70&&89"Grade: B":"Grade: C");
-let score = 85;
-console.log(
-  score >= 90
-    ? "Grade: A"
-    : score >= 70 && score <= 89
-    ? "Grade: B"
-    : "Grade: C"
-);
+// console.log(
+//   score >= 90
+//     ? "Grade: A"
+//     : score >= 70 && score <= 89
+//     ? "Grade: B"
+//     : "Grade: C"
+// );
+
+// Turnary Oprator and Nullish Oprator
+let isLoggedIn;
+
+let username = "user",
+  password = "pass";
+console.log(username, password);
+if (username == "user" && password == "pass") {
+  isLoggedIn = true;
+} else {
+  isLoggedIn = false;
+}
+console.log(isLoggedIn);
+//==> dar inja behtare ma az Turnary Oprator estefade konim 
+let isLoggedIn = username == "user" && password == "pass";
