84a516acd9e10d494be7ada5be645aeb8a7d1df6
Omid Afifi <afifiomid@yahoo.com>
2025-07-01 18:19:15 +0330

    conditions

---
+++ step07-conditions/condition.js
@@ -0,0 +1,8 @@
+//conditions
+//ta javabesh nashe true shart ejra nemishe
+console.log("5 > 1:==>", 5 > 1);
+console.log("1 < 4:==>", 1 < 4);
+console.log("5 >= 5:==>", 5 >= 5);
+console.log("5 <= 5:==>", 5 <= 5);
+console.log("5 < 5 :==>", 5 < 5);
+console.log("5 > 5 :==>", 5 > 5);
