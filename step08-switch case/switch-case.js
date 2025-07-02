15f8550c90dd4a02ff2c87a0da9facedb65fabb4
Omid Afifi <afifiomid@yahoo.com>
2025-07-02 18:19:26 +0330

    Switch Case

---
+++ step08-switch case/switch-case.js
@@ -0,0 +1,16 @@
+//Switch Case in JavaScript
+//switch bedeutet auf persich «تعویض کردن» یا «انتخاب کردن از بین گزینه‌ها»
+//در برنامه‌نویسی، switch یعنی یک ساختار که بر اساس مقدار یک متغیر یا عبارت، یکی از چند مسیر ممکن را انتخاب می‌کند و اجرا می‌کند
+// case یعنی «حالت» یا «گزینه خاص».
+// درون switch، هر case یک مقدار خاص است که اگر مقدار متغیر برابر آن بود، کد مربوط به آن اجرا می‌شود.
+let number = 0;
+
+if (number > 0) {
+  console.log("this number is positiv");
+} else if (number < 0) {
+  console.log("this number is negativ");
+} else {
+  console.log("this number is zero");
+}
+
+
