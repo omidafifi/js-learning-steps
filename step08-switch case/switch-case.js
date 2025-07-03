5d0756c3bcef7b4b076c3c98f5300084ce499fa3
Omid Afifi <afifiomid@yahoo.com>
2025-07-03 19:52:25 +0330

    Switch Case in JavaScript

--- step08-switch case/switch-case.js
+++ step08-switch case/switch-case.js
@@ -3,7 +3,7 @@
 //در برنامه‌نویسی، switch یعنی یک ساختار که بر اساس مقدار یک متغیر یا عبارت، یکی از چند مسیر ممکن را انتخاب می‌کند و اجرا می‌کند
 // case یعنی «حالت» یا «گزینه خاص».
 // درون switch، هر case یک مقدار خاص است که اگر مقدار متغیر برابر آن بود، کد مربوط به آن اجرا می‌شود.
-let number = 0;
+// let number = 0;
 
 // if (number > 0) {
 //   console.log("this number is positiv");
@@ -12,9 +12,18 @@ let number = 0;
 // } else {
 //   console.log("this number is zero");
 // }
-console.log(typeof number === "number");
-if (typeof number === "number") {
-  console.log("this is a number");
-} else if (number >= 0) {
-  console.log("its not a number");
+// console.log(typeof number === "number");
+// if (typeof number === "number") {
+//   console.log("this is a number");
+// } else if (number >= 0) {
+//   console.log("its not a number");
+// }
+let number = 4;
+
+if (number > 0) {
+  console.log("this number is positiv");
+} else if (number < 0) {
+  console.log("this number is negetiv");
+} else {
+  console.log("this number is zero");
 }
