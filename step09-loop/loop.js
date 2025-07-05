6764672697cf20ef67f095e95a6791bc0668545b
Omid Afifi <afifiomid@yahoo.com>
2025-07-05 11:43:02 +0330

    for loop

--- step09-loop/loop.js
+++ step09-loop/loop.js
@@ -1,13 +1,11 @@
 // loop
 //for loop
 
-for (let i = 1; i <= 10; i++) {
+// for (let i = 0; i <= 12; i++) {
+//   console.log(i, "i");
+// }
+
+
+for (let i=23 ; i >17 ; i--){
   console.log(i);
-}
-
-
-
-for (let userName = "omid" ; userName === "omid" , userName++){
-    console.log(userName);
-
 }
