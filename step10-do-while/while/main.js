3d9928fc764e22211da4e50043c30733e25f71df
Omid Afifi <afifiomid@yahoo.com>
2025-07-06 13:37:42 +0330

    do / do-while

---
+++ step10-do-while/while/main.js
@@ -0,0 +1,12 @@
+//do / do-while
+let counter = 0;
+while (counter <= 10) {
+  console.log("Counter :", counter);
+  counter++;
+}
+
+let index = 0;
+do {
+  console.log("Counter :", counter);
+  counter++;
+} while (counter <= 10);
