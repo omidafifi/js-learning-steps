c302ea444631e7e6487997091b32c0dfdd6dfbe5
Omid Afifi <afifiomid@yahoo.com>
2025-07-08 11:37:29 +0330

    functions

---
+++ step11-functions/main.js
@@ -0,0 +1,17 @@
+//functions
+// function sayHello() {
+//   console.log("Hi there ");
+// }
+// sayHello();
+
+// pas 1 function ba kalame kelidi fuction shoru mishe jeloye un bayad parantez baz va baste hatman bashe ke dar un parameter haye ma gharar migire va dar dele fucnction ma ham dasturat ke bayesti amal shavad gharar migire
+// va nokte mohem in ke hatman bayad baadesh function ro seda zad chejuri in ke esme function ro miyarim va hatman bayesti in () dar jelo function gharar girad
+// function name(params) {
+
+// }
+// name==>();
+function log(message) {
+  console.log(message);
+}
+
+log("omid is here");
