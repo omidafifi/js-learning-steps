87f93bb0507c6ee4a74a90811444081c64cdfe8e
Omid Afifi <afifiomid@yahoo.com>
2025-07-08 13:36:17 +0330

    functions

--- step11-functions/main.js
+++ step11-functions/main.js
@@ -15,33 +15,72 @@
 //   console.log(message);
 // }
 // log("omid is here");
-let userInput = 0;
-let temp = 0;
-function primeNumber(userInput) {
-  if (typeof userInput === "number" && userInput > 1) {
-    let temp = 0;
-    for (let i = 1; i <= userInput; i++) {
-      if (userInput % i === 0) {
-        temp++;
-      }
-    }
+// let userInput = 0;
+// let temp = 0;
+// function primeNumber(userInput) {
+//   if (typeof userInput === "number" && userInput > 1) {
+//     let temp = 0;
+//     for (let i = 1; i <= userInput; i++) {
+//       if (userInput % i === 0) {
+//         temp++;
+//       }
+//     }
 
-    if (temp === 2) {
-      console.log("✅ عدد اول است:", userInput);
-    } else {
-      console.log("❌ عدد اول نیست:", userInput);
-    }
-  } else {
-    console.log("⚠️ ورودی معتبر نیست:", userInput);
-  }
-}
+//     if (temp === 2) {
+//       console.log("✅ عدد اول است:", userInput);
+//     } else {
+//       console.log("❌ عدد اول نیست:", userInput);
+//     }
+//   } else {
+//     console.log("⚠️ ورودی معتبر نیست:", userInput);
+//   }
+// }
 
-// تست تابع:
-primeNumber(1); // ❌ عدد اول نیست
-primeNumber(2); // ✅
-primeNumber(3); // ✅
-primeNumber(4); // ❌
-primeNumber(5); // ✅
-primeNumber(6); // ❌
-primeNumber(7); // ✅
-primeNumber("fda"); // ⚠️ ورودی نامعتبر
+// // تست تابع:
+// primeNumber(1); // ❌ عدد اول نیست
+// primeNumber(2); // ✅
+// primeNumber(3); // ✅
+// primeNumber(4); // ❌
+// primeNumber(5); // ✅
+// primeNumber(6); // ❌
+// primeNumber(7); // ✅
+// primeNumber("fda"); // ⚠️ ورودی نامعتبر
+// تابع (Function) مجموعه‌ای از دستورهاست که یک کار خاص را انجام می‌دهد و فقط وقتی اجرا می‌شود که آن را «صدا» بزنی.
+// function name() {
+//   // دستورات
+// }
+// function sayHello() {
+//   console.log("Mein Name ist Omidreza Afifi");
+// }
+// sayHello();
+// 🎯 تمرین ۱:
+// 	1.	تابعی بنویس به نام greetMe
+// 	2.	داخل تابع، عبارت console.log("درود بر من!") را بنویس
+// 	3.	سپس تابع را صدا بزن (اجرا کن)
+// function greetMe() {
+//   console.log("درود بر من!");
+// }
+// greetMe();
+// درس ۲: پارامترها و آرگومان‌ها در توابع
+//	•	پارامتر (Parameter) = متغیری که موقع تعریف تابع استفاده می‌کنی.
+// •	آرگومان (Argument) = مقداری که موقع صدا زدن تابع بهش می‌دی.
+
+// function welcomeUser(firstName) {
+//   console.log("خوش آمدی " + firstName + "!");
+// }
+// welcomeUser("Omidreza");
+// welcomeUser("Erfan");
+
+// تابعی بنویس که دو عدد به عنوان ورودی (پارامتر) بگیره و جمع اون‌ها رو برگردونه (return کنه). سپس نتیجه‌ی برگشتی رو در خارج از تابع چاپ کن.
+// function sayHello() {
+//   console.log("hallo omidreza");
+
+// }
+
+// function sayMyName() {
+//   return "omidreza";
+// }
+// let myMessage = sayMyName();
+// console.log(myMessage);
+
+//
