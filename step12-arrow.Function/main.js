01e7d7bdde9d3737f905bd4b1e51da34c1d0738f
Omid Afifi <afifiomid@yahoo.com>
2025-07-08 19:23:33 +0330

    Arrow Function

---
+++ step12-arrow.Function/main.js
@@ -0,0 +1,24 @@
+//Arrow Function
+//Arrow Function یعنی نوع جدیدتر و خلاصه‌تری برای تعریف تابع در جاوااسکریپت، که با فلش => نوشته می‌شه.
+
+// function sayhello() {
+//   console.log("سلام");
+// }
+// sayhello();
+
+// const sayhello = () => {
+//   console.log("سلام");
+// };
+
+// sayhello();
+const userName = (firstName, lastName) => {
+  return firstName + ` ` + lastName;
+};
+let result = userName("omidreza", "afifi");
+console.log(result);
+
+const greetUser = (name) => {
+  console.log("Guten Tag Herr" + ` ` + name + " !");
+};
+
+greetUser("Afifi");
