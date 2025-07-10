2a5a3f4a3b274076e10cdaaeb2aec5fb2ea1575b
Omid Afifi <afifiomid@yahoo.com>
2025-07-10 14:10:57 +0330

    Sum of array elements using a loop

--- step14-splice/main.js
+++ step14-splice/main.js
@@ -1,30 +1,37 @@
-//Splice
-// let noten = [18, 15, 3, 20, 18.5];
-// console.log(noten);
-// for (const item of noten) {
-//   //->	•	وظیفه‌اش اینه که روی یک مجموعه (مثل آرایه یا رشته) حرکت کنه و به هر عنصر دسترسی بده.
-//   console.log(item); //-> 	•	هیچ کاری خودش انجام نمی‌ده مگر اینکه در داخلش دستوراتی بنویسی (مثلاً console.log())
-// }
+// //Splice
+// // let noten = [18, 15, 3, 20, 18.5];
+// // console.log(noten);
+// // for (const item of noten) {
+// //   //->	•	وظیفه‌اش اینه که روی یک مجموعه (مثل آرایه یا رشته) حرکت کنه و به هر عنصر دسترسی بده.
+// //   console.log(item); //-> 	•	هیچ کاری خودش انجام نمی‌ده مگر اینکه در داخلش دستوراتی بنویسی (مثلاً console.log())
+// // }
 
+// // let temp = 0;
+// // for (const item2 of noten) {
+// //   temp += item2;
+// // }
+// // console.log(temp);
+// // let names = ["ali", " javad ", " mohammadreza ", "erfan"];
+// // console.log(names);
+// // // names.splice(1, 2);
+// // // names.splice(1, 1);
+// // // names.splice(2, 0), "milad", "hassan";
+// // const middOfArray = names.length / 2;
+// // names.splice(middOfArray, 0, "Omid", "Reza");
+// // console.log(names);
+
+// // let userNames = [
+// //   "Omidrzaf@gmail.com",
+// //   "Zahraafifi@gmail.com",
+// //   "zahra.yzp@yahoo.com",   ==> for of (loop)
+// // ];
+// // for (const item of userNames) {
+// //   console.log(item);
+// // }
+// //Sum of array elements using a loop
+// let noten = [10, 3, 6, 16, 13, 12, 20];
 // let temp = 0;
-// for (const item2 of noten) {
-//   temp += item2;
+// for (const itemNoten of noten) {
+//   temp += itemNoten;
 // }
 // console.log(temp);
-// let names = ["ali", " javad ", " mohammadreza ", "erfan"];
-// console.log(names);
-// // names.splice(1, 2);
-// // names.splice(1, 1);
-// // names.splice(2, 0), "milad", "hassan";
-// const middOfArray = names.length / 2;
-// names.splice(middOfArray, 0, "Omid", "Reza");
-// console.log(names);
-
-let userNames = [
-  "Omidrzaf@gmail.com",
-  "Zahraafifi@gmail.com",
-  "zahra.yzp@yahoo.com",
-];
-for (const item of userNames) {
-  console.log(item);
-}
