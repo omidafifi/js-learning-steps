4e8a6467da732941c3dcbb8183c290929053c8d4
Omid Afifi <afifiomid@yahoo.com>
2025-07-12 16:50:50 +0330

    ✅ Practice for...in loop on objects + dynamic access and comparison notes
    - Added for...in loop example to iterate over object keys
    - Used dynamic property access with object[key] in a loop
    - Demonstrated 'in' operator to check key existence
    - Added examples to explain why [] == [] and {} == {} return false

---
+++ step20-forIn/main.js
@@ -0,0 +1,31 @@
+//For in
+// for...in یک حلقه مخصوص پیمایش کلیدهای (keys) یک Object است.
+// let person = {
+//   name: "omid",
+//   age: 34,
+//   city: "shiraz",
+// };
+// for (const key in person) {
+//   console.log(key);
+// }
+
+let user = {
+  name: "Omid",
+  age: 34,
+};
+console.log(user.name);
+
+const codes = {
+  98: "IRI",
+  44: "UK",
+  1: "USA",
+};
+console.log("98" in codes);
+for (const code in codes) {
+  console.log("+" + code, ":", codes[code]);
+}
+
+// nokte 2 object va 2 ta array ro nemishe ba ham moghayese kard = false mide be ma
+
+console.log([] == []);
+console.log({} == {});
