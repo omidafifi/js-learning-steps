e8c1ce864a518a689e37040cf1bafc9c33258429
Omid Afifi <afifiomid@yahoo.com>
2025-07-13 11:57:57 +0330

    feat: add example of for...in loop with dynamic key access using bracket notation

--- step22.obj.methods/main.js
+++ step22.obj.methods/main.js
@@ -1,1 +1,12 @@
 //object methods
+
+let user = {
+  id: 123115412,
+  firstName: "omid",
+  lastName: "afifi",
+  age: 34,
+};
+const is = Object.keys(user);
+const is1 = Object.values(user);
+console.log(is);
+console.log(is1);
