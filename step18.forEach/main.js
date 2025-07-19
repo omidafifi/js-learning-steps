36480f96cd8cbbfc5ec75bc1c64a397e5e47835e
Omid Afifi <afifiomid@yahoo.com>
2025-07-20 01:27:04 +0330

    foreach

--- step18.forEach/main.js
+++ step18.forEach/main.js
@@ -1,19 +1,43 @@
-//forEachs Method
-// // متد forEach() روی آرایه اجرا می‌شه و به ازای هر عنصر آرایه، یک تابع (callback) رو صدا می‌زنه.
-// array.forEach((element) => {
-//   //دستوراتی که برای هر عنصری انجام میشود
+// //forEachs Method
+// // // متد forEach() روی آرایه اجرا می‌شه و به ازای هر عنصر آرایه، یک تابع (callback) رو صدا می‌زنه.
+// // array.forEach((element) => {
+// //   //دستوراتی که برای هر عنصری انجام میشود
+// // });
+// let number = [1, 2, 3, 4];
+// number.forEach((element) => {
+//   console.log("number:", element);
 // });
-let number = [1, 2, 3, 4];
-number.forEach((element) => {
-  console.log("number:", element);
-});
 
-let names = ["Omidreza", "hassan", " Mohammad"];
-names.forEach((element) => {
-  console.log("سلام خدا بر :", element);
-});
+// let names = ["Omidreza", "hassan", " Mohammad"];
+// names.forEach((element) => {
+//   console.log("سلام خدا بر :", element);
+// });
 
-let langs = ["Node.js", "CSS", "JS"];
-langs.forEach((element) => {
-  console.log("من در حال یادگیری HTML هستم و… :", element);
-});
+// let langs = ["Node.js", "CSS", "JS"];
+// langs.forEach((element) => {
+//   console.log("من در حال یادگیری HTML هستم و… :", element);
+// });
+
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// // for (let i = 0; i <= 10; i++) {
+// //   if (i <= 5) {
+// //     continue;
+// //   }
+// //   console.log(i);
+// // }
+
+// for (const item of numbers) {
+//   if (item < 5) {
+//     console.log(item);
+//   }
+// }
+// array.forEach(element => {
+
+// });
+let colors = ["red ", "green ", "blue ", "white"];
+colors.forEach((item) => console.log(item));
+console.log("----------------");
+const animals = ["cat", "dog", "rabbit"];
+// هر کدام از حیوانات را با forEach و Arrow Function چاپ کن.
+animals.forEach((element) => console.log(element));
