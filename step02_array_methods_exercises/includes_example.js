fe6138ca36d249bcafe0034988230347cb465cdc
Omid Afifi <afifiomid@yahoo.com>
2025-07-22 21:46:29 +0330

    includes

--- step02_array_methods_exercises/includes_example.js
+++ step02_array_methods_exercises/includes_example.js
@@ -1,6 +1,9 @@
-let heute = ["html", "css", "javascript"];
-if (heute.includes("javascript")) {
-  console.log("JavaScript ist verfügbar");
-} else {
-  console.log("nix");
-}
+// let heute = ["html", "css", "javascript"];
+// if (heute.includes("javascript")) {
+//   console.log("JavaScript ist verfügbar");
+// } else {
+//   console.log("nix");
+// }
+const numbers = [2, 4, 6, 8];
+let incNum = numbers.includes(3);
+console.log(incNum);
