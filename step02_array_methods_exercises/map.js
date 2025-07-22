5bb317da2b349591e7235b9577abd5b36c3a7875
Omid Afifi <afifiomid@yahoo.com>
2025-07-22 22:00:22 +0330

    feat: add examples demonstrating map() method for string case conversion and number multiplication

--- step02_array_methods_exercises/map.js
+++ step02_array_methods_exercises/map.js
@@ -1,13 +1,19 @@
-//map
-//map() روی هر عنصر آرایه یک تابع اجرا می‌کند و آرایه‌ای جدید می‌سازد که شامل نتایج اجرای آن تابع است.
-let skills = ["js", "Nodejs", "mangodb", "reactjs", "html", "css"];
+// //map
+// //map() روی هر عنصر آرایه یک تابع اجرا می‌کند و آرایه‌ای جدید می‌سازد که شامل نتایج اجرای آن تابع است.
+// let skills = ["js", "Nodejs", "mangodb", "reactjs", "html", "css"];
 
-let newArray = skills.map((item) => item.toLowerCase());
-let newArray1 = skills.map((item) => item.toUpperCase());
-console.log(newArray);
-console.log(newArray1);
-let numbers = [1, 3, 5, 6, 7, 8, 9];
-let multi = numbers.map((item) => item * 2);
-console.log(numbers);
-console.log(multi);
-//feat: add examples demonstrating map() method for string case conversion and number multiplication
+// let newArray = skills.map((item) => item.toLowerCase());
+// let newArray1 = skills.map((item) => item.toUpperCase());
+// console.log(newArray);
+// console.log(newArray1);
+// let numbers = [1, 3, 5, 6, 7, 8, 9];
+// let multi = numbers.map((item) => item * 2);
+// console.log(numbers);
+// console.log(multi);
+// //feat: add examples demonstrating map() method for string case conversion and number multiplication
+
+const numbers = [1, 2, 3];
+const doubled = numbers.map((num) => {
+  return num * 2;
+});
+console.log(doubled);
