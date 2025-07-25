8fc7e080185bb2d2263f7dda3ea4fc662578b48a
Omid Afifi <afifiomid@yahoo.com>
2025-07-25 22:51:32 +0330

    total in rest

--- step 25-Rest.Spead/main.js
+++ step 25-Rest.Spead/main.js
@@ -7,3 +7,14 @@ function showNames(...names) {
 showNames("Ali ", "Sara", "Reza");
 
 // ...names  Yaani har chi arguman hast biad berize to in array
+
+function sumAll(...numbers) {
+  let total = 0;
+  for (const n of numbers) {
+    total += n;
+  }
+  return total;
+}
+console.log(sumAll(3, 5, 10));
+//inja tabe(function) ma har tedad adadi ke begiri ba ham jam mikone
+
