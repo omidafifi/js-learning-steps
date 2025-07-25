e5e9c7b02abdf6654033ce104349eab0dab30fb7
Omid Afifi <afifiomid@yahoo.com>
2025-07-25 22:42:40 +0330

    Rest and Spread training

---
+++ step 25-Rest.Spead/main.js
@@ -0,0 +1,9 @@
+//Rest and Spread
+// rest Oprators
+
+function showNames(...names) {
+  console.log(names);
+}
+showNames("Ali ", "Sara", "Reza");
+
+// ...names  Yaani har chi arguman hast biad berize to in array
