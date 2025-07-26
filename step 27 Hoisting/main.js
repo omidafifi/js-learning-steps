c6aface1e8388ebb21ea085d1114dab43e812bbd
Omid Afifi <afifiomid@yahoo.com>
2025-07-26 12:06:28 +0330

    // ✅ Commit: Added explanation and examples for Execution Context, Scope, Scope Chain, and IIFE in JavaScript.

---
+++ step 27 Hoisting/main.js
@@ -0,0 +1,12 @@
+//Hoisting
+
+Nodejs();
+
+console.log(programmingLanguage);
+
+function Nodejs(params) {
+  console.log("Node js curse ");
+}
+
+ programmingLanguage = "JavaScript";
+//// ✅ Commit: Added explanation and examples for Execution Context, Scope, Scope Chain, and IIFE in JavaScript.
