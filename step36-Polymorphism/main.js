4788acde1b8a91e2c483e126b9f6d8613a474493
Omid Afifi <afifiomid@yahoo.com>
2025-07-28 19:00:36 +0330

    "Implement polymorphism with Animal, Dog, and Cat classes using makeSound method"

--- step36-Polymorphism/main.js
+++ step36-Polymorphism/main.js
@@ -1,26 +1,26 @@
 //Polymorphism
 class Animal {
-    makeSound() {
-      console.log("Some generic animal sound");
-    }
+  makeSound() {
+    console.log("Some generic animal sound");
   }
-  
-  class Dog extends Animal {
-    makeSound() {
-      console.log("Bark");
-    }
+}
+
+class Dog extends Animal {
+  makeSound() {
+    console.log("Bark");
   }
-  
-  class Cat extends Animal {
-    makeSound() {
-      console.log("Meow");
-    }
+}
+
+class Cat extends Animal {
+  makeSound() {
+    console.log("Meow");
   }
-  
-  function playSound(animal) {
-    animal.makeSound(); // polymorphic behavior
-  }
-  
-  const animals = [new Dog(), new Cat(), new Animal()];
-  
-  animals.forEach(playSound);
+}
+
+function playSound(animal) {
+  animal.makeSound(); // polymorphic behavior
+}
+
+const animals = [new Dog(), new Cat(), new Animal()];
+
+animals.forEach(playSound);
