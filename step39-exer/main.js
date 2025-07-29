f9c37518d52bcfec0b8edb2fc6d8d779692c1ab5
Omid Afifi <afifiomid@yahoo.com>
2025-07-29 22:55:05 +0330

    reject .catch

--- step39-exer/main.js
+++ step39-exer/main.js
@@ -1,16 +1,12 @@
 // Promis -async-await
-// console.log("دوم باید اجرا باشه ");
-
-// setTimeout(() => {
-//   console.log("اول باید اجرا باشه ");
-// }, 1000);
-// console.log("دوم باید اجرا باشه ");
 
 const promise = new Promise((resolve, reject) => {
   setTimeout(() => {
-    console.log("اول باید اجرا باشه ");
-    resolve();
+    console.log("اول اجرا شود");
+    // resolve();
+    reject();
   }, 1000);
 });
 
-promise.then(() => console.log("دوم باید اجرا باشه "));
+// promise.then(() => console.log("دوم اجرا شود"));
+promise.catch(() => console.log("با خطا روبه رو شدیم"));
