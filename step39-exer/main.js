141aba49025c886670ffee32117fda1bac4b7b4f
Omid Afifi <afifiomid@yahoo.com>
2025-07-30 10:51:31 +0330

    rename .then param and correct typos in log messages

--- step39-exer/main.js
+++ step39-exer/main.js
@@ -1,30 +1,20 @@
 // Promis -async-await
 
-// const promise = new Promise((resolve, reject) => {
-//   setTimeout(() => {
-//     console.log("اول اجرا شود");
-//     // resolve();
-//     reject();
-//   }, 1000);
-// });
-
-// console.log(promise); //=> pending
-// // promise.then(() => console.log("دوم اجرا شود")); //=> fullfilled
-// // promise.catch(() => console.log("با خطا روبه رو شدیم"));==>rejected
-
 const randomNumber = new Promise((resolve, reject) => {
   setTimeout(() => {
-    const randomNumber = Math.random; //randomnumber forgot and Math.random
-    if (randomNumber < 20) {
-      resolve(randomNumber);
-      // console.log("عدد :", randomNumber);
+    const number = Math.random() * 2 + 1;
+    if (number < 2) {
+      resolve(number);
     } else {
-      reject("An Error Ocured");
-      // console.log("عدد :", randomNumber);
+      reject("عدد بزرگتر از ۲ بود");
     }
-  }, 2000);
+  }, 1000);
 });
 
 randomNumber
-  .then((result) => console.log(result).then((num) => num * 10))
-  .catch((error) => console.log(error)); // ""
+  .then((result) => {
+    console.log("Success", result);
+  })
+  .catch((error) => {
+    console.log("Error", error);
+  });
